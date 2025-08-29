5e9aa3e6058963d985bb1336bc393f33b7bd4e6b
lyxer123 <42857073+lyxer123@users.noreply.github.com>
2025-08-29 23:16:44 +0800

    删除了很多重复功能的bat文件

--- run_mqtt_fix_cn.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-powershell -ExecutionPolicy Bypass -File "%~dp0fix_mqtt_connection_cn.ps1"
--- run_mqtt_fix_en.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-powershell -ExecutionPolicy Bypass -File "%~dp0fix_mqtt_connection_en.ps1"
